2c15df0f5c84a092be62999adaa4cd89198174b8
Nicward69 <ant-no.pen@live.com.mx>
2023-09-26 11:04:53 -0500

    Add files via upload

---
+++ Estudios-05-09-23/main.java
@@ -0,0 +1,21 @@
+//int intARGB = 0;
+        // a = 255, r = 247, g = 177, b = 24
+        //intARGB = intARGB | 0b11111111;
+        //System.out.println("Valor ARGB: " + intARGB + "bits "+ Integer.toBinaryString(intARGB));
+        
+        //intARGB = intARGB << 8;
+        //System.out.println("Valor ARGB: " + intARGB + "bits: " + Integer.toBinaryString(intARGB));
+        
+        //intARGB = intARGB | 0xF7; //247 // 0b 1111 0111
+        //System.out.println("Valor ARGB: " + intARGB + "bits: " + Integer.toBinaryString(intARGB));
+        
+        //intARGB = intARGB << 8;
+        //System.out.println("Valor ARGB: " + intARGB + "bits: " + Integer.toBinaryString(intARGB));
+        
+        //intARGB = intARGB | 177; //0xB  //0b 1011 0001
+        //intARGB = intARGB << 8;
+        //System.out.println("Valor ARGB: " + intARGB + "bits: " + Integer.toBinaryString(intARGB));
+        
+        //intARGB = intARGB | 24; //0x18  //0b 0001 1000    
+        //ya no hay desplazamiento porque ya terminamos la secuencia de los numeros
+        //System.out.println("Valor ARGB: " + intARGB + "bits: " + Integer.toBinaryString(intARGB));
